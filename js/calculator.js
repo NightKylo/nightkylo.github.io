5f516439415612dee6837b82010fa0cc8a14517a
NightKylo <67463721+NightKylo@users.noreply.github.com>
2020-12-19 14:35:17 +0100

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,24 @@
 /*
-    calculator.js
-    Default calculator handling
-    Copyright © 2020 Marius Kraus (NightKylo)
+    /js/calculator.js
+    JavaScript calculator
 */
 
 const output_field = document.getElementById('output');
+document.onkeydown = handle_keypress;
 
 window.addEventListener('load', function(){
     console.log('Window loaded');
-    console.log('');
+    console.log(' ');
 
     if(window.innerWidth <= 425){
         document.getElementById('copy_button').className = 'unselectable invisible';
     }
 
     if(window.innerWidth >= 1850){
+        console.log('updated');
         document.getElementById('help_button').style.float = 'none';
         document.getElementById('help_button').style.setProperty('margin-left', '-1500px', 'important');
     }
-
 });
 
 document.getElementById('button0').addEventListener('click', function(){
@@ -84,34 +84,6 @@ document.getElementById('button/').addEventListener('click', function(){
 document.getElementById('button=').addEventListener('click', function(){
     output = output_field.innerHTML;
     
-    if(output == ''){
-        return;
-    }
-
-    if(output == '30 + 04 + 2006'){
-        if(index == colors.length - 1){
-            index = -1;
-            rewrite('');
-            if(window.innerWidth > 425){
-                document.getElementById('copy_button').style.marginTop = "-37px";
-                document.getElementById('buttonc').style.marginTop = "-35px";
-            }
-            else{
-                document.getElementById('buttonc').style.marginTop = "-35px";
-            }
-        }
-        else{
-            index++;
-        }
-        draw_background(index, true, true);
-        return;
-    }
-
-    for(i = 0; i < output.length; i++){
-        if(output.charAt(i) == '='){
-            return;
-        }
-    }
     counter = 0;
     calculation = output_field.innerHTML;
 
@@ -131,8 +103,9 @@ document.getElementById('button=').addEventListener('click', function(){
             return;
         }
     }
-
-    calculate(list.join(''));
+    console.log(list.join(''));
+    result = calculate(list.join(''));
+    console.log('"result":"' + result + '"');
 });
 
 function calculate(calculation){
@@ -141,13 +114,7 @@ function calculate(calculation){
     signs = [];
     add = [];
     nums = [];
-    line_signs = [];
-    new_nums = [];
-    index = -1;
     result = 0;
-    a = 0;
-    c = 0;
-    last_result = 0;
 
     //console.log('');
     //console.log('started calculation: ' + calculation);
@@ -189,9 +156,6 @@ function calculate(calculation){
     }
 
     if(signs.length == 0){
-        result = nums[0];
-        console.log('"result":"' + result + '"');
-        write(' = ' + result);
         return;
     }
     
@@ -202,145 +166,32 @@ function calculate(calculation){
     */
 
     //The result is calculated here
-    if(!calculation.includes('×') && !calculation.includes('÷')){
-        for(i = 0; i < signs.length; i++){
-            //console.log(signs[i])
-            if(signs[i] == '+'){
-                if(i == 0){
-                    result = parseFloat(nums[i], 10) + parseFloat(nums[i + 1], 10);
-                }
-                else{
-                    result += parseFloat(nums[i + 1], 10);
-                }
-            }
-            else{
-                if(i == 0){
-                    result = parseFloat(nums[i], 10) - parseFloat(nums[i + 1], 10);
-                }
-                else{
-                    result -= parseFloat(nums[i + 1], 10);
-                }
-            }
+    for(i = 0; i < signs.length; i++){
+        //console.log(signs[i])
+        if(signs[i] == '+'){
+            result = parseFloat(nums[i], 10) + parseFloat(nums[i + 1], 10);
         }
-    }
-    else if(!calculation.includes('+') && !calculation.includes('-')){
-        for(i = 0; i < signs.length; i++){
-            //console.log(signs[i])
-            if(signs[i] == '×'){
-                if(i == 0){
-                    result = parseFloat(nums[i], 10) * parseFloat(nums[i + 1], 10);
-                }
-                else{
-                    result *= parseFloat(nums[i + 1], 10);
-                }
-            }
-            else{
-                if(i == 0){
-                    result = parseFloat(nums[i], 10) / parseFloat(nums[i + 1], 10);
-                }
-                else{
-                    result /= parseFloat(nums[i + 1], 10);
-                }
-            }
+        else if (signs[i] == '-'){
+            result = parseFloat(nums[i], 10) - parseFloat(nums[i + 1], 10);
         }
-    }
-    else{
-        for(i = 0; i < signs.length; i++){
-            if(signs[i] != '×' && signs[i] != '÷'){
-                line_signs.push(signs[i]);
-            }
+        else if (signs[i] == '×'){
+            result = parseFloat(nums[i], 10) * parseFloat(nums[i + 1], 10);
         }
-
-        //console.log('Nums: ' + nums);
-        //console.log('Signs: ' + signs);
-
-        for(i = 0; i < signs.length; i++){
-            if(signs[i] == '×'){
-                if(a == 0){
-                    last_result = parseFloat(nums[i]) * parseFloat(nums[i + 1])
-                    new_nums.push(last_result);
-                    //console.log('Pushed succesfully');
-                }
-                else{
-                    index = new_nums.findIndex(function(value){ return value == last_result; });
-                    if(index != -1){
-                        new_nums[index] = last_result * parseFloat(nums[i + 1], 10);
-                        last_result *= parseFloat(nums[i + 1], 10);
-                    }
-                    else{
-                        //console.error('Error, index of ' + last_result + ' not found');
-                    }
-                }
-                a++;
-                //console.log('New Nums: ' + new_nums);
-            }
-            else if(signs[i] == '÷'){
-                if(a == 0){
-                    last_result = parseFloat(nums[i]) / parseFloat(nums[i + 1])
-                    new_nums.push(last_result);
-                    //console.log('Pushed succesfully');
-                }
-                else{
-                    index = new_nums.findIndex(function(value){ return value == last_result; });
-                    if(index != -1){
-                        new_nums[index] = last_result / parseFloat(nums[i + 1], 10);
-                        last_result /= parseFloat(nums[i + 1], 10);
-                    }
-                    else{
-                        //console.error('Error, index of ' + last_result + ' not found');
-                    }
-                }
-                a++;
-                //console.log('New Nums: ' + new_nums);
-            }
-            else{
-                c++;
-                l = i + c
-
-                if(nums[l] == null){
-                    for(b = 0; b < nums.length; b++){
-                        l = b;
-                    }
-                }
-
-                //console.log('Push index: ' + l);
-                new_nums.push(parseFloat(nums[l]));
-            }
-            index = -1;
-        }
-        
-        //console.log('Nums: ' + nums);
-        //console.log('Final_new_nums: ' + new_nums);
-        //console.log('Final line_signs: ' + line_signs);
-
-        for(i = 0; i < line_signs.length; i++){
-            if(line_signs[i] == '+'){
-                if(i == 0){
-                    result = new_nums[i] + new_nums[i + 1];
-                }
-                else{
-                    result += new_nums[i + 1];
-                }
-            }
-            else{
-                if(i == 0){
-                    result = new_nums[i] - new_nums[i + 1];
-                }
-                else{
-                    result -= new_nums[i + 1];
-                }
-            }
+        else if (signs[i] == '÷'){
+            result = parseFloat(nums[i], 10) / parseFloat(nums[i + 1], 10);
         }
     }
 
-    console.log('"result":"' + result + '"');
-    write(' = ' + result);
+    rewrite(result);
+    return result;
 }
 
 function enter(content){
+    signs = ['+', '-', '×', '÷'];
     output = output_field.innerHTML;
     list = [];
     counter = 0;
+    changed_sign = false;
 
     if(window.innerWidth > 425){
         document.getElementById('copy_button').style.marginTop = "-61px";
@@ -358,24 +209,142 @@ function enter(content){
         }
     }
 
-    //Searching a result entered before
-    for(i = 0; i < output.length; i++){
-        if(output.charAt(i) == '='){
-            start_number = '';
-            for(a = i + 2; a < output.length; a++){
-                start_number += output[a];
+    //Checking if the last sign in the calculation is a sign and if it is, changing it to the new given sign
+    //console.log("out:", output, ", con:", content)
+    signs.forEach(sign => {
+        if(output.charAt(output.length - 2) == sign && content.length == 3){
+            // console.log("out:", output, ", con:", content);
+            new_output = "";
+            for(i = 0; i < output.length; i++){
+                if(i < output.length - 3){
+                    new_output += output[i];
+                }
             }
-            
-            if(start_number != 'Nan'){
-                rewrite(start_number);
-            }
-            else{
-                rewrite('');
-            }
-            break;
+            rewrite(new_output);
+            changed_sign = true;
         }
+    });
+
+    // Calculate a result, if the calculation contains a sign
+    if(content.length > 1 && !changed_sign){
+        signs.forEach(sign => {
+            if(list.join('').includes(sign)){
+                calculate(list.join(''));
+                return
+            }
+        });
     }
     write(content);
+}
+
+function handle_keypress(key){
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    //console.log(key.code);
+    if(key.code == 'NumpadEnter' || key.code == 'Enter'){
+        output = output_field.innerHTML;
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) == '='){
+                return;
+            }
+        }
+        counter = 0;
+        calculation = output_field.innerHTML;
+    
+        list = [];
+    
+    
+        for (i = 0; i < calculation.length; i++) {
+            if(calculation.charAt(i) != ' '){
+                list[counter] = calculation.charAt(i);
+                counter++;
+            }
+        }
+    
+        for (i = 0; i < calculation.length; i++) {
+            if(calculation.charAt(i) == '/' || calculation.charAt(i) == '<' || calculation.charAt(i) == '(' || calculation.charAt(i) == 's' || calculation.charAt(i) == '?'){
+                rewrite('You are offically the worst hacker visited this site ever! Train your skills, when you want to hack websites!');
+                return;
+            }
+        }
+    
+        result = calculate(list.join(''));
+        console.log('"result":"' + result + '"');
+    }
+
+    if(key.code == 'Backspace' || key.code == 'Delete'){
+        rewrite('');
+        if(window.innerWidth > 425){
+            document.getElementById('copy_button').style.marginTop = "-37px";
+            document.getElementById('buttonc').style.marginTop = "-35px";
+        }
+        else{
+            document.getElementById('buttonc').style.marginTop = "-35px";
+        }
+        return;
+    }
+
+    if(key.code == 'KeyC'){
+        output = output_field.innerHTML;
+        list = [];
+
+        if(output.lengt == 0){
+            return;
+        }
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) != ' '){
+                list.push(output.charAt(i));
+            }
+        }
+    
+        output = list.join('');
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) == '='){
+                result = output.split('=', 2)[1];
+                //console.log(result);
+                copyToClipboard(result);
+                return;
+            }
+        }
+
+        copyToClipboard(output);
+    }
+
+    if(key.code == 'KeyV'){
+        output = output_field.innerHTML;
+        pasteClipboard();
+    }
+
+    for(i = 0; i < key.code.length; i++){
+        if(key.code.charAt(i) == 'F'){
+            break;
+        }
+        for(a = 0; a < nums.length; a++){
+            if(key.code.charAt(i) == nums[a]){
+                enter(a);
+                return;
+            }
+        }
+    }
+
+    if(key.code == 'NumpadAdd'){
+        enter(' + ');
+    }
+    else if(key.code == 'NumpadSubtract'){
+        enter(' - ');
+    }
+    else if(key.code == 'NumpadMultiply'){
+        enter(' × ');
+    }
+    else if(key.code == 'NumpadDivide'){
+        enter(' ÷ ');
+    }
+
+    if(key.code == "Comma" || key.code == "Period"){
+        enter('.');
+    }
 }
 
 function write(content){
