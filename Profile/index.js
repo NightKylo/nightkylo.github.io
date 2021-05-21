34413329de5aa4d8030d607e1b712f173d3839db
cracksii <67463721+NightKylo@users.noreply.github.com>
2021-05-21 17:43:40 +0200

    Add files via upload

--- Profile/index.js
+++ Profile/index.js
@@ -1,17 +1,18 @@
 document.getElementById("upload").onchange = loadFile;
 
-function loadFile () {
+function loadFile() {
     var file, img;
-    if (file = this.files[0]) {
+    if(file = this.files[0]) {
         img = new Image();
         var objectUrl = URL.createObjectURL(file);
         img.onload = function () {
             console.log(this.width + " " + this.height);
             if(this.width != this.height)
-                alert("The image is not in a squared form. You may experience weird behavoiur. Cut the image to a square to get the most realistic view.");
+                alert("The image is not in a squared shape. You may experience weird behavoiur. Cut the image to a square to get the most realistic view.");
             for(i = 0; i < document.getElementsByClassName("img").length; i++)
                 document.getElementsByClassName("img")[i].src = objectUrl;
             URL.revokeObjectURL(objectUrl);
         };
+        img.src = objectUrl;
     }
 }
