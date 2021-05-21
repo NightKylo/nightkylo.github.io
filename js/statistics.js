8e2ab35552ddf98f90eed9cf7b9c9c5a48731bf0
cracksii <67463721+NightKylo@users.noreply.github.com>
2021-05-21 12:20:24 +0200

    Delete js directory

--- js/statistics.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function(){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    console.log('width: ' + width + ', height : ' + height);
-});
