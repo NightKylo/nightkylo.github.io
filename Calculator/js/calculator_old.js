ca2f300dff723aa4e4fad88a33e0f092ffb878cc
cracksii <67463721+NightKylo@users.noreply.github.com>
2021-05-21 12:21:44 +0200

    Add files via upload

---
+++ Calculator/js/calculator_old.js
@@ -0,0 +1,470 @@
+/*
+    calculator.js
+    Default calculator handling
+*/
+
+const output_field = document.getElementById('output');
+document.onkeydown = handle_keypress;
+
+window.addEventListener('load', function(){
+    console.log('Window loaded');
+    console.log('');
+
+    if(window.innerWidth <= 425){
+        document.getElementById('copy_button').className = 'unselectable invisible';
+    }
+
+    if(window.innerWidth >= 1850){
+        console.log('updated');
+        document.getElementById('help_button').style.float = 'none';
+        document.getElementById('help_button').style.setProperty('margin-left', '-1500px', 'important');
+    }
+});
+
+document.getElementById('button0').addEventListener('click', function(){
+    enter('0');
+});
+document.getElementById('button1').addEventListener('click', function(){
+    enter('1');
+});
+document.getElementById('button2').addEventListener('click', function(){
+    enter('2');
+});
+document.getElementById('button3').addEventListener('click', function(){
+    enter('3');
+});
+document.getElementById('button4').addEventListener('click', function(){
+    enter('4');
+});
+document.getElementById('button5').addEventListener('click', function(){
+    enter('5');
+});
+document.getElementById('button6').addEventListener('click', function(){
+    enter('6');
+});
+document.getElementById('button7').addEventListener('click', function(){
+    enter('7');
+});
+document.getElementById('button8').addEventListener('click', function(){
+    enter('8');
+});
+document.getElementById('button9').addEventListener('click', function(){
+    enter('9');
+});
+
+document.getElementById('button1000').addEventListener('click', function(){
+    enter('000');
+});;
+document.getElementById('button.').addEventListener('click', function(){
+    enter('.');
+})
+document.getElementById('buttonc').addEventListener('click', function(){
+    rewrite('');
+    if(window.innerWidth > 425){
+        document.getElementById('copy_button').style.marginTop = "-37px";
+        document.getElementById('buttonc').style.marginTop = "-35px";
+    }
+    else{
+        document.getElementById('buttonc').style.marginTop = "-35px";
+    }
+});
+
+document.getElementById('button+').addEventListener('click', function(){
+    enter(' + ');
+});
+document.getElementById('button-').addEventListener('click', function(){
+    enter(' - ');
+});
+document.getElementById('buttonx').addEventListener('click', function(){
+    enter(' &times; ');
+});
+document.getElementById('button/').addEventListener('click', function(){
+    enter(' &divide; ');
+});;
+document.getElementById('button=').addEventListener('click', function(){
+    output = output_field.innerHTML;
+    
+    for(i = 0; i < output.length; i++){
+        if(output.charAt(i) == '='){
+            return;
+        }
+    }
+    counter = 0;
+    calculation = output_field.innerHTML;
+
+    list = [];
+
+
+    for (i = 0; i < calculation.length; i++) {
+        if(calculation.charAt(i) != ' '){
+            list[counter] = calculation.charAt(i);
+            counter++;
+        }
+    }
+
+    for (i = 0; i < calculation.length; i++) {
+        if(calculation.charAt(i) == '/' || calculation.charAt(i) == '<' || calculation.charAt(i) == '(' || calculation.charAt(i) == 's' || calculation.charAt(i) == '?'){
+            rewrite('You are offically the worst hacker visited this site ever! Train your skills, when you want to hack websites!');
+            return;
+        }
+    }
+
+    calculate(list.join(''));
+});
+
+function calculate(calculation){
+    calculate_signs = ['+', '-', '×', '÷', '='];
+    input_chars = [];
+    signs = [];
+    add = [];
+    nums = [];
+    line_signs = [];
+    new_nums = [];
+    index = -1;
+    result = 0;
+    a = 0;
+    c = 0;
+    last_result = 0;
+
+    //console.log('');
+    //console.log('started calculation: ' + calculation);
+    
+    //The Array will be filled with empty strings
+    for(i = 0; i < calculation.length; i++){
+        input_chars[i] = '';
+    }
+    
+    //The numbers will be seperated from the inputs
+    for(i = 0; i < calculation.length; i++){
+        //console.log(calculation[i])
+        if(!calculate_signs.includes(calculation[i])){
+            input_chars[i] = calculation[i];
+        }
+        else{
+            //console.log(calculation[i]);
+            signs.push(calculation[i]);
+        }
+    }
+
+    //The numbers are connected to real numbers
+    for(i = 0; i < input_chars.length; i++){
+        if(input_chars[i] != '' || i == input_chars.length - 1){
+            add.push(input_chars[i]);
+            //console.log('Current add: ' + add)
+
+            if(i == input_chars.length - 1){
+                //console.log('Add: ' + add)
+                nums.push(add.join(''));
+                add = [];
+            }
+        }
+        else{
+            //console.log('Add: ' + add)
+            nums.push(add.join(''));
+            add = [];
+        }
+    }
+
+    if(signs.length == 0){
+        result = nums[0];
+        console.log('"result":"' + result + '"');
+        write(' = ' + result);
+        return;
+    }
+    
+    /*Debug messages
+    console.log('Input_chars: ' + input_chars);
+    console.log('Signs: ' + signs);
+    console.log('Numbers: ' + nums);
+    */
+
+    //The result is calculated here
+    if(!calculation.includes('×') && !calculation.includes('÷')){
+        for(i = 0; i < signs.length; i++){
+            //console.log(signs[i])
+            if(signs[i] == '+'){
+                if(i == 0){
+                    result = parseFloat(nums[i], 10) + parseFloat(nums[i + 1], 10);
+                }
+                else{
+                    result += parseFloat(nums[i + 1], 10);
+                }
+            }
+            else{
+                if(i == 0){
+                    result = parseFloat(nums[i], 10) - parseFloat(nums[i + 1], 10);
+                }
+                else{
+                    result -= parseFloat(nums[i + 1], 10);
+                }
+            }
+        }
+    }
+    else if(!calculation.includes('+') && !calculation.includes('-')){
+        for(i = 0; i < signs.length; i++){
+            //console.log(signs[i])
+            if(signs[i] == '×'){
+                if(i == 0){
+                    result = parseFloat(nums[i], 10) * parseFloat(nums[i + 1], 10);
+                }
+                else{
+                    result *= parseFloat(nums[i + 1], 10);
+                }
+            }
+            else{
+                if(i == 0){
+                    result = parseFloat(nums[i], 10) / parseFloat(nums[i + 1], 10);
+                }
+                else{
+                    result /= parseFloat(nums[i + 1], 10);
+                }
+            }
+        }
+    }
+    else{
+        for(i = 0; i < signs.length; i++){
+            if(signs[i] != '×' && signs[i] != '÷'){
+                line_signs.push(signs[i]);
+            }
+        }
+
+        //console.log('Nums: ' + nums);
+        //console.log('Signs: ' + signs);
+
+        for(i = 0; i < signs.length; i++){
+            if(signs[i] == '×'){
+                if(a == 0){
+                    last_result = parseFloat(nums[i]) * parseFloat(nums[i + 1])
+                    new_nums.push(last_result);
+                    //console.log('Pushed succesfully');
+                }
+                else{
+                    index = new_nums.findIndex(function(value){ return value == last_result; });
+                    if(index != -1){
+                        new_nums[index] = last_result * parseFloat(nums[i + 1], 10);
+                        last_result *= parseFloat(nums[i + 1], 10);
+                    }
+                    else{
+                        //console.error('Error, index of ' + last_result + ' not found');
+                    }
+                }
+                a++;
+                //console.log('New Nums: ' + new_nums);
+            }
+            else if(signs[i] == '÷'){
+                if(a == 0){
+                    last_result = parseFloat(nums[i]) / parseFloat(nums[i + 1])
+                    new_nums.push(last_result);
+                    //console.log('Pushed succesfully');
+                }
+                else{
+                    index = new_nums.findIndex(function(value){ return value == last_result; });
+                    if(index != -1){
+                        new_nums[index] = last_result / parseFloat(nums[i + 1], 10);
+                        last_result /= parseFloat(nums[i + 1], 10);
+                    }
+                    else{
+                        //console.error('Error, index of ' + last_result + ' not found');
+                    }
+                }
+                a++;
+                //console.log('New Nums: ' + new_nums);
+            }
+            else{
+                c++;
+                l = i + c
+
+                if(nums[l] == null){
+                    for(b = 0; b < nums.length; b++){
+                        l = b;
+                    }
+                }
+
+                //console.log('Push index: ' + l);
+                new_nums.push(parseFloat(nums[l]));
+            }
+            index = -1;
+        }
+        
+        //console.log('Nums: ' + nums);
+        //console.log('Final_new_nums: ' + new_nums);
+        //console.log('Final line_signs: ' + line_signs);
+
+        for(i = 0; i < line_signs.length; i++){
+            if(line_signs[i] == '+'){
+                if(i == 0){
+                    result = new_nums[i] + new_nums[i + 1];
+                }
+                else{
+                    result += new_nums[i + 1];
+                }
+            }
+            else{
+                if(i == 0){
+                    result = new_nums[i] - new_nums[i + 1];
+                }
+                else{
+                    result -= new_nums[i + 1];
+                }
+            }
+        }
+    }
+
+    console.log('"result":"' + result + '"');
+    write(' = ' + result);
+}
+
+function enter(content){
+    output = output_field.innerHTML;
+    list = [];
+    counter = 0;
+
+    if(window.innerWidth > 425){
+        document.getElementById('copy_button').style.marginTop = "-61px";
+        document.getElementById('buttonc').style.marginTop = "-59px";
+    }
+    else{
+        document.getElementById('buttonc').style.marginTop = "-59px";
+    }
+
+    //Removing all whiespaces from the string
+    for (i = 0; i < output.length; i++) {
+        if(output.charAt(i) != ' '){
+            list[counter] = output.charAt(i);
+            counter++;
+        }
+    }
+
+    //Searching a result entered before
+    for(i = 0; i < output.length; i++){
+        if(output.charAt(i) == '='){
+            start_number = '';
+            for(a = i + 2; a < output.length; a++){
+                start_number += output[a];
+            }
+            
+            if(start_number != 'Nan'){
+                rewrite(start_number);
+            }
+            else{
+                rewrite('');
+            }
+            break;
+        }
+    }
+    write(content);
+}
+
+function handle_keypress(key){
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    //console.log(key.code);
+
+    if(key.code == 'NumpadEnter' || key.code == 'Enter'){
+        output = output_field.innerHTML;
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) == '='){
+                return;
+            }
+        }
+        counter = 0;
+        calculation = output_field.innerHTML;
+    
+        list = [];
+    
+    
+        for (i = 0; i < calculation.length; i++) {
+            if(calculation.charAt(i) != ' '){
+                list[counter] = calculation.charAt(i);
+                counter++;
+            }
+        }
+    
+        for (i = 0; i < calculation.length; i++) {
+            if(calculation.charAt(i) == '/' || calculation.charAt(i) == '<' || calculation.charAt(i) == '(' || calculation.charAt(i) == 's' || calculation.charAt(i) == '?'){
+                rewrite('You are offically the worst hacker visited this site ever! Train your skills, when you want to hack websites!');
+                return;
+            }
+        }
+    
+        calculate(list.join(''));
+    }
+
+    if(key.code == 'Backspace' || key.code == 'Delete'){
+        rewrite('');
+        if(window.innerWidth > 425){
+            document.getElementById('copy_button').style.marginTop = "-37px";
+            document.getElementById('buttonc').style.marginTop = "-35px";
+        }
+        else{
+            document.getElementById('buttonc').style.marginTop = "-35px";
+        }
+        return;
+    }
+
+    if(key.code == 'KeyC'){
+        output = output_field.innerHTML;
+        list = [];
+
+        if(output.lengt == 0){
+            return;
+        }
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) != ' '){
+                list.push(output.charAt(i));
+            }
+        }
+    
+        output = list.join('');
+    
+        for(i = 0; i < output.length; i++){
+            if(output.charAt(i) == '='){
+                result = output.split('=', 2)[1];
+                //console.log(result);
+                copyToClipboard(result);
+                return;
+            }
+        }
+
+        copyToClipboard(output);
+    }
+
+    if(key.code == 'KeyV'){
+        output = output_field.innerHTML;
+        pasteClipboard();
+    }
+
+    for(i = 0; i < key.code.length; i++){
+        if(key.code.charAt(i) == 'F'){
+            break;
+        }
+        for(a = 0; a < nums.length; a++){
+            if(key.code.charAt(i) == nums[a]){
+                enter(a);
+                return;
+            }
+        }
+    }
+
+    if(key.code == 'NumpadAdd'){
+        enter(' + ');
+    }
+    else if(key.code == 'NumpadSubtract'){
+        enter(' - ');
+    }
+    else if(key.code == 'NumpadMultiply'){
+        enter(' × ');
+    }
+    else if(key.code == 'NumpadDivide'){
+        enter(' ÷ ');
+    }
+}
+
+function write(content){
+    output_field.innerHTML += content;
+}
+
+function rewrite(content){
+    output_field.innerHTML = content;
+}
---
+++ Calculator/js/core.js
@@ -0,0 +1,135 @@
+/*
+    /js/core.js
+    cookie setting,
+    background loading,
+    responsibility
+*/ 
+
+const help_url = 'https://nightkylo.github.io/help';
+const colors = [
+    'linear-gradient(0deg, #004BFA 0%, #00123B 100%)',
+    'linear-gradient(0deg, #8000FF 0%, #200040 100%)',
+    'linear-gradient(0deg, #F8A255 0%, #E14B56 100%)',
+    'linear-gradient(0deg, #28F7D1 0%, #32ADF0 100%)',
+    'linear-gradient(0deg, #00CC7E 0%, #0DFCE4 100%)',
+    'linear-gradient(0deg, #222559 0%, #F2293A 100%)',
+    'linear-gradient(0deg, #000000 0%, #FF0000 100%)'
+]
+
+window.addEventListener('load', function(){
+    var paint_objects = [];
+    if(!checkcookie('background')){
+        setcookie('background', '-1', 365);
+    }
+    else{
+        var background = readcookie('background');
+        console.log(background);
+        if(background != '-1'){
+            paint_objects.push(document.getElementsByTagName('main')[0]);
+            for(i = 0; i < document.getElementsByClassName('bg-circle').length; i++){
+                paint_objects.push(document.getElementsByClassName('bg-circle')[i]);
+            }
+            for(i = 0; i < paint_objects.length; i++){
+                paint_objects[i].style.background = colors[background];
+            }
+        }
+    }
+    
+    /* This code is only relevant for local testing
+
+    background = colors[5];
+    paint_objects.push(document.getElementsByTagName('main')[0]);
+    for(i = 0; i < document.getElementsByClassName('bg-circle').length; i++){
+        paint_objects.push(document.getElementsByClassName('bg-circle')[i]);
+    }
+    for(i = 0; i < paint_objects.length; i++){
+        paint_objects[i].style.background = background;
+    }*/
+});
+
+document.getElementById('copy_button').addEventListener('click', function(){
+    output = document.getElementById('output').innerHTML;
+    list = [];
+
+    if(output.lengt == 0){
+        return;
+    }
+
+    for(i = 0; i < output.length; i++){
+        if(output.charAt(i) != ' '){
+            list.push(output.charAt(i));
+        }
+    }
+
+    output = list.join('');
+
+    for(i = 0; i < output.length; i++){
+        if(output.charAt(i) == '='){
+            result = output.split('=', 2)[1];
+            //console.log(result);
+            copyToClipboard(result);
+            return;
+        }
+    }
+
+    copyToClipboard(output);
+});
+
+document.getElementById('help_button').addEventListener('click', function(){
+    window.location.href = help_url;
+});
+
+function copyToClipboard(str){
+    navigator.clipboard.writeText(str);
+}
+
+function pasteClipboard(){
+    navigator.clipboard.readText().then(text => write(text));
+
+    function write(text){
+        console.log("Pasting clipboard", text);
+        if(text != ''){
+            if(window.innerWidth > 425){
+                document.getElementById('copy_button').style.marginTop = "-61px";
+                document.getElementById('buttonc').style.marginTop = "-59px";
+            }
+            else{
+                document.getElementById('buttonc').style.marginTop = "-59px";
+            }
+            document.getElementById('output').innerHTML += text;
+        }
+    }
+}
+
+function setcookie(name, content, expiredate){
+    var d = new Date();
+    d.setTime(d.getTime() + (expiredate * 24 * 60 * 60 * 1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = name + "=" + content + ";" + expires + ";path=/";
+    console.log('Created cookie ' + name + ' with the value ' + content);
+}
+
+function readcookie(name){
+    var cookiename = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookiename) == 0){
+            return c.substring(cookiename.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkcookie(name){
+    var cookie = readcookie(name);
+    if (cookie != ""){
+        return true;
+    }else {
+        return false;
+    }
+}
